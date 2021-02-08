35e11b966ea8e12ee9cccfe84e238b4905314566
enzo <enzo.wintz1@outlook.fr>
2021-02-08 14:47:44 +0100

    v2

--- TD1.go
+++ TD1.go
@@ -1,4 +1,5 @@
 //TD1
+//exo1
 /*
 package main
 
@@ -8,3 +9,32 @@ func main() {
     fmt.Println("hello world")
 }
 */
+//exo2
+/*
+package main
+import "fmt"
+func main() {
+fmt.Printf( "Bad ") ;
+fmt.Println("formating" )
+}
+*/
+
+//exo3
+package main
+
+import (
+    "flag"
+    "fmt"
+)
+
+const VERSION = "1.0"
+
+func main() {
+    var version = flag.Bool("version", false, "Version prog")
+    flag.Parse()
+    if *version {
+        fmt.Println(VERSION)
+    } else {
+        fmt.Println("pas de version")
+    }
+}
