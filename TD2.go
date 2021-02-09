d5733ff0895f62b83b030ce9370a35d3ccaca418
enzo <enzo.wintz1@outlook.fr>
2021-02-09 16:51:54 +0100

    T

--- TD2.go
+++ TD2.go
@@ -16,7 +16,7 @@ func main() {
 */
 
 
-
+/*
 package main
 
 import "fmt"
@@ -37,3 +37,70 @@ func main() {
         fmt.Printf("%v: %v\n", name, ip)
     }
 }
+*/
+/*
+package main
+
+import (
+    "fmt"
+    "strconv"
+	"strings"
+)
+
+type IPAddr [4]byte
+
+/*func (ip IPAddr) String() string {
+    return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
+}*/
+
+/*
+func (ip IPAddr) String() string {
+    s := make([]string, len(ip))
+    for i, val := range ip {
+        s[i] = strconv.Itoa(int(val))
+    }    
+    return strings.Join(s, ".")
+}
+
+
+func main() {
+    hosts := map[string]IPAddr{
+        "loopback":  {127, 0, 0, 1},
+        "googleDNS": {8, 8, 8, 8},
+		
+    }
+
+    //With FMT
+    for name, ip := range hosts {
+        fmt.Printf("%v:%v\n", name, ip)
+    }
+
+
+    //With StrConv
+
+}
+*/
+
+package main
+
+import (
+    "fmt"
+	"time"
+
+)
+
+func run() error{
+    return MyError{
+       When: time.now(),
+       What: "erreur",
+    }
+ } 
+ 
+ type MyError struct {
+    When time.Time
+    What string
+ }
+ 
+ func (err MyError) Error() string{
+    return fmt.Sprintf("ERROR: At: %v this happened: %s", err.When, err.What)
+ }
